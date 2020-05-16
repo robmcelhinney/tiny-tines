1d33c7158e63c5e21290aec86fd716729fbbbcce
robmcelhinney <robmcelhinney@hotmail.com>
2020-05-17 00:54:58 +0100

    Add more tests. Tidy up cody.

--- tines.py
+++ tines.py
@@ -13,6 +13,7 @@ class FormatValue():
 class formatting_dict(dict):
     def __getitem__(self, name):
         value = self.get(name)
+		
         if isinstance(value, dict):
             # rewrap nested dictionaries to handle missing nested keys
             value = type(self)(value)
@@ -24,6 +25,10 @@ def main():
 	arg_par.add_argument("-j", "--json", required=True,
 			help="path to facial json of actions")
 	args = vars(arg_par.parse_args())
+	tines(args)
+
+
+def tines(args):
 	try:
 		with open(args["json"], 'r') as f:
 			data = f.read()
@@ -32,17 +37,14 @@ def main():
 	except Exception as e:
 		print("Failed: ", e)
 
-	obj = json.loads(data)
-
-	# print(obj)
-
-	agents = obj["agents"]
-
+	try:
+		obj = json.loads(data)
+	except ValueError:
+		print("Failed to decode json.")
+		return
 	events = {}
 
-	for agent in agents:
-		# print("agent: ", agent)
-		# print("events: ", events)
+	for agent in obj["agents"]:
 		if agent["type"] == "HTTPRequestAgent":
 			http_request_agent(events, agent)
 			
@@ -59,13 +61,18 @@ def http_request_agent(events, agent):
 	except requests.ConnectionError:
 		print("failed to connect to url: ", url)
 		return
+	except Exception as e:
+		print("Could not connect to url")
+		return
 	if not res.ok:
 		print(f'status code: {res.status_code} outside of 2xx range')
 		return
 
-	# if (req.status_code)
-	res_text = json.loads(res.text)
-	# print("response: ", response)
+	try:
+		res_text = json.loads(res.text)
+	except ValueError:
+		print("Failed to decode json.")
+		return
 	events[agent["name"]] = res_text
 
 def print_agent(events, agent):
@@ -78,27 +85,25 @@ def print_agent(events, agent):
 def interpolate_option(events, str):
 	# Create array of values that are surrounded by '{{' '}}'
 	bracket_values = [p.split('}}')[0] for p in str.split('{{') if '}}' in p]
-	print("\nbracket_values!!!!!!: ", bracket_values)
 
 	# If values are surrounded by brackets enter
 	if len(bracket_values) > 0:
-		str = convert_dot_notation(bracket_values, str)
-		
-		# Need to escape any left over double parantheses so that it 
-		# remains printed as is.
-		 
-		print("str before format map: ", str)
+		converted_values = convert_dot_paran(bracket_values)
+		str = add_conv_back(converted_values, str)
+
+		print("interpolate_option str: ", str)
+
 		# Use f strings to replace converted value with
 		str = str.format_map(formatting_dict(events))
 		
-
+		# Need to escape any left over double parantheses so that it 
+		# remains printed as is.
 		str = escape_brackets(str)
 
 	return str
 
 
-
-def convert_dot_notation(bracket_values, str):
+def convert_dot_paran(bracket_values):
 	# Convert dot notation to parantheses
 	converted_values = []
 	for value in bracket_values:
@@ -106,43 +111,40 @@ def convert_dot_notation(bracket_values, str):
 		value = value.replace(".", "][")
 
 		converted_values.append(value + "]")
-	print("converted_values: ", converted_values)
+	return converted_values
 
+
+def add_conv_back(converted_values, str):
 	# Loop through converted_values and replace originals one at a time
-	print(" 1 convert_dot_notation str: ", str)
-	regex = '\{{.*?\}}'
+	regex = '\{{[^{]*?\}}'
 	for conv_value in converted_values:
 		# Substitute first {{value}} with {conv_value}
 		str = re.sub(regex, "{" + conv_value + "}", str, 1)
-
-	print("2 convert_dot_notation str: ", str)
 	return str
 
 
 def escape_brackets(str):
-	print("start escape : ", str)
-	close_matches = []
-	def repl_close(m):
-		close_matches.append(m.group())
-		return ("{}{}").format(m.group(), "}")
-
-	
+	# Add extra starting/closing bracket to any group of one or more 
+	# starting/closing bracket
 	start_matches = []
 	def repl_start(m):
 		start_matches.append(m.group())
 		return ("{}{}").format(m.group(), "{")
 
+	close_matches = []
+	def repl_close(m):
+		close_matches.append(m.group())
+		return ("{}{}").format(m.group(), "}")
+
+	# Escape starting brackets
+	pattern = re.compile(r'\{{1,}')
+	str = re.sub(pattern, repl_start, str)
+
+	# Escape closing brackets
 	pattern = re.compile(r'\}{1,}')
 	str = re.sub(pattern, repl_close, str)
 
-	pattern = re.compile(r'\{{1,}')
-	str = re.sub(pattern, repl_start, str)
-	
-	print("end escape : ", str)
 	return str
-	# close_bracked_regex = '\}}{1,}'
-	# print("re search: ", re.search(close_bracked_regex, str))
-	# str = re.sub(close_bracked_regex, re.search(close_bracked_regex, str) + "}", str)
 
 
 if __name__ == "__main__":
--- tines_test.py
+++ tines_test.py
@@ -15,13 +15,42 @@ class Tines(unittest.TestCase):
             ("Hello {{foo.bar}}!", "Hello World!"),
             ("Hello {{foo.bar}!", "Hello {{foo.bar}!"),
             ("Hello {{foo.bar}} }}!", "Hello World }}!"),
-            ("Hello {{ {{foo.bar}}!", "{{ Hello World!"),
+            ("Hello {{ {{foo.bar}}!", "Hello {{ World!"),
             ("Hello {{foo.qux}}!", "Hello !"))
 
+
     def test_interpolate_option(self):
-        for option, result_option in self.options:
-            result = tines.interpolate_option(self.events, option)
-            self.assertEqual(result_option, result)
+        for option, result in self.options:
+            result_inter = tines.interpolate_option(self.events, option)
+            self.assertEqual(result, result_inter)
+
+
+    bracket_values = ((["test.testing"], ["test[testing]"]),
+                    (["dot.notation", "convert.me"], ["dot[notation]", "convert[me]"]))
+
+    def test_convert_dot_paran(self):
+        for value, result in self.bracket_values:
+            parantheses_result = tines.convert_dot_paran(value)
+            self.assertEqual(result, parantheses_result)
+
+    converted_options = ((["test[testing]"], "domain/?a={{test.testing}}", "domain/?a={test[testing]}"),
+            (["convert[me]", "dot[not]"], "domain/?a={{convert.me}}&b={{dot.not}}", "domain/?a={convert[me]}&b={dot[not]}"),
+    )
+    def test_add_conv_back(self):
+        for conv_values, value_str, result in self.converted_options:
+            conv_result = tines.add_conv_back(conv_values, value_str)
+            self.assertEqual(result, conv_result)
+   
+    escape_brackets = (("{test}", "{{test}}"),
+            ("test", "test"),
+            ("{ test", "{{ test"),
+            ("{ test }} }", "{{ test }}} }}"))
+
+    def test_escape_brackets(self):
+        for str, result in self.escape_brackets:
+            result_escape = tines.escape_brackets(str)
+            self.assertEqual(result, result_escape)
+
 
 if __name__ == '__main__':
     unittest.main()
